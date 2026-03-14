c49a262947af33c8416b0925c4b0c6af37b170bf
Nick Begg <nick@stunttruck.net>
2026-03-14 17:51:03 +0100

    more fixture work

--- tests/fixtures/mode_time.h
+++ tests/fixtures/mode_time.h
@@ -8,10 +8,21 @@
 
 #include <stdio.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 int mode_time(int argc, char *argv[]) {
   (void)argc; /* Unused parameter */
   (void)argv; /* Unused parameter */
+
+  // we don't actually want to run if this is launched as root; 
+  // we want the settimeofday/adjtime calls to fail, rather than
+  // actually set the time
+  if (getuid() == 0) {
+      fprintf(stderr, "cowardly refusing to run as root\n");
+      return -1;
+  }
 
   /* === gettimeofday() - get date and time === */
   {
@@ -27,13 +38,32 @@ int mode_time(int argc, char *argv[]) {
       settimeofday(&tv, &tz);
   }
 
+  const char* tempfile = "/tmp/strace_futimes_test.txt";
+  /* === futimes() === */
+  {
+      int fd = open(tempfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      if (fd < 0) {
+          fprintf(stderr, "couldn't create tempfile for time tests\n");
+      }
+
+      struct timeval tv[2];
+      futimes(fd, tv);
+      close(fd);
+  }
+
   /* === utimes() === */
   {
       struct timeval tv[2];
-      utimes("somefile", tv);
+      utimes(tempfile, tv);
   }
 
-  printf("DPNE\n");
+  /* === adjtime === */
+  {
+      struct timeval delta = {1, 2};
+      struct timeval olddelta = {0, 0};
+      adjtime(&delta, &olddelta);
+  }
+  
   return 0;
 }
 
